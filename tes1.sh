c926d962cbfc1bef62001ba0bd7729ce77ccfc3b
ucupcap4 <72310722+ucupcap4@users.noreply.github.com>
2020-10-04 09:34:30 +0700

    New file

---
+++ tes1.sh
@@ -0,0 +1,41 @@
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo
+toilet -f big -F gay by saiful
+echo $green"selamat datang"
+echo
+sleep 2
+echo $yellow"silakan pilih tools nya"
+echo
+echo $cyan"1.) setabilkan jaringan"
+echo
+echo $blue"0.) keluar"
+echo
+echo $white
+read -p "pilih mana : " brow
+
+if  [ $brow = 1 ] || [ $brow = 1 ]
+then
+clear
+toilet -f big -F gay saiful
+echo $green"setabilkan jaringan by saiful"
+sleep 2
+ping -s1000 1.1.1.1
+fi
+
+if [ $brow = 0 ]  ||  [ $brow = 0 ]
+then
+clear
+echo $yellow
+figlet "Bye"
+echo $white"To Be Continued"
+
+sleep 3
+exit
+fi
